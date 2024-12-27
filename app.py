550ded5be80e8a4d138b8c6cbd14a17083cd3720
Jerusha <jerusharanitamiri8663@gmail.com>
2024-12-27 22:39:56 +0530

    Explicitly downloading punkt and stopwords to app

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import time
 nltk_data_path = 'D:/machinelearning/spamsmsdetection/nltk_data'
 os.environ['NLTK_DATA'] = nltk_data_path
 
-nltk.download('punkt')
+nltk.download('punkt_tab')
 nltk.download('stopwords')
 
 # Preprocessing function
